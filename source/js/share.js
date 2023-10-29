0553da19105a84b9d188bc75607665454e89b5d2
baifengxiao <javaer@111.com>
2023-10-29 15:38:59 +0800

    魔改部分，更新完butterfly会被覆盖 魔改（五）:白天黑夜不同背景图片,vue+element样式弹窗，按键防抖，带来源的信息分享

---
+++ source/js/share.js
@@ -0,0 +1,41 @@
+// 防抖全局计时器
+let TT = null;    //time用来控制事件的触发
+// 防抖函数:fn->逻辑 time->防抖时间
+function debounce(fn, time) {
+    if (TT !== null) clearTimeout(TT);
+    TT = setTimeout(fn, time);
+}
+
+// 分享本页
+function share_() {
+    let url = window.location.origin + window.location.pathname
+    try {
+        // 截取标题
+        var title = document.title;
+        var subTitle = title.endsWith("| 零星") ? title.substring(0, title.length - 14) : title;
+        navigator.clipboard.writeText('零星的站内分享\n标题：' + subTitle + '\n链接：' + url + '\n欢迎来访！');
+        new Vue({
+            data: function () {
+                this.$notify({
+                    title: "成功复制分享信息🎉",
+                    message: "您现在可以通过粘贴直接跟小伙伴分享了！",
+                    position: 'top-left',
+                    offset: 50,
+                    showClose: true,
+                    type: "success",
+                    duration: 5000
+                });
+                // return { visible: false }
+            }
+        })
+    } catch (err) {
+        console.error('复制失败！', err);
+    }
+    // new ClipboardJS(".share", { text: function () { return '标题：' + document.title + '\n链接：' + url } });
+    // btf.snackbarShow("本页链接已复制到剪切板，快去分享吧~")
+}
+
+// 防抖
+function share() {
+    debounce(share_, 300);
+}
